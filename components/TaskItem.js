7500579a2d2a1b8a4dfaa7847d8d89a67af965c2
Erasmo Jordan <erasmo7jordan@gmail.com>
2023-06-09 15:27:46 -0300

    Actualiza styles

--- components/TaskItem.js
+++ components/TaskItem.js
@@ -9,13 +9,14 @@ const TaskItem = ({ task, handleDelete }) => {
     return (
         <View style={styles.itemContainer}>
             <TouchableOpacity
+                style={styles.touchableContainer}
                 onPress={() => navigation.navigate('TaskFormScreen', { id:task.id })}>
                 <Text style={styles.itemTitle}>{task.title}</Text>
-                <Text style={styles.itemTitle}>{task.description}</Text>
+                <Text style={styles.itemDescription}>{task.description}</Text>
             </TouchableOpacity>
         
             <TouchableOpacity 
-                style={{backgroundColor: "#ee5253", padding: 7, borderRadius:5}}
+                style={styles.touchableBtnDelete}
                 onPress={() => handleDelete(task.id)}
             >
                 <Text>Delete</Text>
@@ -32,10 +33,24 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center'
+        alignItems: 'center',
+        borderColor:'#3f3f3f',
+        borderWidth:1
     },
     itemTitle: {
         color: "#ffffff"
+    },
+    itemDescription: {
+        color: "#b2b2b2",
+        fontSize:13
+    },
+    touchableContainer: {
+        width: "72%"
+    },
+    touchableBtnDelete: {
+        backgroundColor: "#ee5253", 
+        padding: 7, 
+        borderRadius:5
     }
 }) 
 
